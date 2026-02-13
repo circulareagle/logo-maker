b0bcc704016ada943af58422d47a4a4e5978ae56
Circular Eagle <242617042+circulareagle@users.noreply.github.com>
2026-02-13 15:53:00 +0100

    Add file

---
+++ DelegationService.java
@@ -0,0 +1,14 @@
+package MintLogo.App;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
